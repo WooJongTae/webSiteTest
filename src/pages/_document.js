8445d45227f801c4eb734f9c53848ed41bfc11d4
woojongtae <binily@naver.com>
2024-01-05 23:04:56 +0900

    notion 추가

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
-      <Head />
+    <Html lang="en">
+      <Head>
+        <title>오늘도 해보자 할 수 있어</title>
+        <meta name="description" content="오늘도 해보자" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,6 @@
 import Layout from "@/components/layout";
+import axios from "axios";
+import { DATABASE_ID, TOKEN } from "../../config/index.js";
 
 export default function Projects() {
   return (
@@ -6,4 +8,28 @@ export default function Projects() {
       <h1>프로젝트</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const options = {
+    method: "POST",
+    url: `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    data: { page_size: 100 },
+  };
+
+  const response = await axios.request(options);
+  const ProjectsIds = response.data.results.map(
+    (aProject) => aProject.properties.이름.title.plain_text
+  );
+  console.log(ProjectsIds);
+
+  return {
+    props: {},
+  };
 }
